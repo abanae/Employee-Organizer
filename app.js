ca7133a0fcd3befaa887d522322c7b407eaf10a4
Ana Ea <aabad0464@gmail.com>
2021-07-14 11:01:23 -0600

    setting conncetion to mysql

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 // SEtting Dependencies
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+
+// Setting Connection mySql
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'employee_trackerDB',
+  });
