39e1483054e2189df36a6d24032412cee9fc19ac
Ana Ea <aabad0464@gmail.com>
2021-07-14 01:02:19 -0600

    creating department table

--- seed.sql
+++ seed.sql
@@ -2,3 +2,12 @@
 DROP DATABASE IF EXISTS 
 
 CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
