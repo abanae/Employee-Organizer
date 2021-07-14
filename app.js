b4dbfc51eb716c5b62145f110b802b1badb7a672
Ana Ea <aabad0464@gmail.com>
2021-07-14 11:37:48 -0600

    setting connection DB & user prompts

--- app.js
+++ app.js
@@ -9,4 +9,36 @@ const connection = mysql.createConnection({
     user: 'root',
     password: 'password',
     database: 'employee_trackerDB',
-  });
+});
+
+//   Connection DB
+connection.connect(async (err) => {
+    if (err) throw err;
+    console.log(`Welcome to Employee Tracker ${connection.threadId}\n`);
+    try {
+        // Prompt Function
+        const userChoice1 = await inquirer.prompt([
+            [
+                name: 'userOptions',
+                type: 'list',
+                message: 'What would you like to do?',
+                choices: [
+                    'View All Employees',
+                    'View All Employees By Roles',
+                    'View all Employees By Deparments',
+                    'View all Employees by Manager',
+                    'Add Employee?',
+                    'Add Role?',
+                    'Add Department',
+                    'Update Employee Roles',
+                    'Update Employee Managers',
+                    'Delete Employee',
+                    'Delete Role',
+                    'Delete Department',
+                    'View Department Budgets'
+                ],
+            ]
+        ]);
+    }
+
+});
