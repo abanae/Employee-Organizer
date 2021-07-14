f4ae8e5b4dcad33c3fee78c3f063f67977aad79d
Ana Ea <aabad0464@gmail.com>
2021-07-14 01:44:54 -0600

    added rows to department table

--- seed.sql
+++ seed.sql
@@ -30,3 +30,11 @@ CREATE TABLE employee (
     FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
+
+
+-- Creates new rows in department table in all named columns
+INSERT INTO department (name)
+VALUES('Sales'),
+('Accounting'),
+('Marketing'),
+('HR');
